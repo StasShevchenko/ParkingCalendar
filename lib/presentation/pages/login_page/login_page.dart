c6a30aaa984c678a3cef86c2f290519f11752260
selegeon <vladgb71@gmail.com>
2023-07-30 22:03:16 +0300

    add adaptive for login screen

--- lib/presentation/pages/login_page/login_page.dart
+++ lib/presentation/pages/login_page/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:parking_project/presentation/responsive/responsive_determ.dart';
 
 import '../../theme/app_colors.dart';
 
@@ -14,85 +15,227 @@ class LoginPage extends StatelessWidget {
       height: 35,
     );
 
-    final Widget carImage = SvgPicture.asset(
-      'assets/images/car_image.svg'
-    );
+    final Widget carImage = SvgPicture.asset('assets/images/car_image.svg');
 
     return Scaffold(
       backgroundColor: AppColors.background,
-      appBar: AppBar(
-        leading: Center(child: companyIcon),
-        title: const Text('Авторизация'),
-      ),
-      body: Column(
-        children: [
-           Expanded(
-            flex: 2,
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: carImage,
-              ),
+      appBar: ResponsiveWidget.isSmallScreen(context)
+          ? AppBar(
+              leading: Center(child: companyIcon),
+              title: const Text('Авторизация'),
+            )
+          : null,
+      body: ResponsiveWidget.isLargeScreen(context) //проверка для адаптивности
+          ? LoginWidgetDesktop(carImage: carImage)
+          : ResponsiveWidget.isMediumScreen(context)
+              ? LoginWidgetTab(carImage: carImage)
+              : LoginWidgetPhone(carImage: carImage),
+    );
+  }
+}
+
+class LoginWidgetPhone extends StatelessWidget {
+  const LoginWidgetPhone({
+    super.key,
+    required this.carImage,
+  });
+
+  final Widget carImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          flex: 2,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: carImage,
             ),
           ),
-          Expanded(
-            flex: 3,
-            child: Container(
-              height: double.maxFinite,
-              width: double.maxFinite,
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(32),
-                  topRight: Radius.circular(32),
-                ),
-              ),
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(32.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Text('Добро пожаловать!', style: Theme.of(context).textTheme.titleLarge, textAlign: TextAlign.center,),
-                      const SizedBox(height: 30,),
-                      TextField(
-                        textInputAction: TextInputAction.next,
-                        onTapOutside: (_) => FocusScope.of(context).unfocus(),
-                        decoration: InputDecoration(
-                          prefixIconColor: AppColors.primaryBlue,
-                          prefixIcon: const Padding(
-                            padding: EdgeInsets.only(left: 8.0,),
-                            child: Icon(Icons.mail),
-                          ),
-                          labelText: 'Введите почту'
-                        ),
-                      ),
-                      const SizedBox(height: 16,),
-                      TextField(
-                        textInputAction: TextInputAction.next,
-                        onTapOutside: (_) => FocusScope.of(context).unfocus(),
-                        obscureText: true,
-                        decoration: InputDecoration(
-                            prefixIconColor: AppColors.primaryBlue,
-                            prefixIcon: const Padding(
-                              padding: EdgeInsets.only(left: 8.0,),
-                              child: Icon(Icons.lock),
-                            ),
-                            labelText: 'Введите пароль'
-                        ),
-                      ),
-                      const SizedBox(height: 16,),
-                      const Align(child: Text('Забыли пароль?'), alignment: Alignment.centerRight,),
-                      const SizedBox(height: 16,),
-                      ElevatedButton(onPressed: (){}, child: const Text('Войти')),
-                    ],
-                  ),
-                ),
+        ),
+        Expanded(
+          flex: 3,
+          child: Container(
+            height: double.maxFinite,
+            width: double.maxFinite,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(32),
+                topRight: Radius.circular(32),
               ),
             ),
-          )
-        ],
+            child: const LoginSectionWidget(),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class LoginWidgetTab extends StatelessWidget {
+  const LoginWidgetTab({
+    super.key,
+    required this.carImage,
+  });
+
+  final Widget carImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 1,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(height: 600, width: 600, child: carImage),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            height: double.maxFinite,
+            width: double.maxFinite,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(32),
+                topRight: Radius.circular(32),
+              ),
+            ),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: LoginSectionWidget(),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class LoginSectionWidget extends StatelessWidget {
+  const LoginSectionWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Добро пожаловать!',
+              style: Theme.of(context).textTheme.titleLarge,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            TextField(
+              textInputAction: TextInputAction.next,
+              onTapOutside: (_) => FocusScope.of(context).unfocus(),
+              decoration: InputDecoration(
+                  prefixIconColor: AppColors.primaryBlue,
+                  prefixIcon: const Padding(
+                    padding: EdgeInsets.only(
+                      left: 8.0,
+                    ),
+                    child: Icon(Icons.mail),
+                  ),
+                  labelText: 'Введите почту'),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            TextField(
+              textInputAction: TextInputAction.next,
+              onTapOutside: (_) => FocusScope.of(context).unfocus(),
+              obscureText: true,
+              decoration: InputDecoration(
+                  prefixIconColor: AppColors.primaryBlue,
+                  prefixIcon: const Padding(
+                    padding: EdgeInsets.only(
+                      left: 8.0,
+                    ),
+                    child: Icon(Icons.lock),
+                  ),
+                  labelText: 'Введите пароль'),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            const Align(
+              child: Text('Забыли пароль?'),
+              alignment: Alignment.centerRight,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton(onPressed: () {}, child: const Text('Войти')),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class LoginWidgetDesktop extends StatelessWidget {
+  const LoginWidgetDesktop({
+    super.key,
+    required this.carImage,
+  });
+
+  final Widget carImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 2,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(height: 600, width: 600, child: carImage),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            height: double.maxFinite,
+            width: double.maxFinite,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(32),
+                topRight: Radius.circular(32),
+              ),
+            ),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: LoginSectionWidget(),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
