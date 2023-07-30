9a22ff4123e84cf255fad8444c96b3b3ca7dccd5
Stas <dark.knight22832@gmail.com>
2023-07-30 18:02:07 +0300

    Mobile version of login page is created

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:parking_project/presentation/pages/login_page/login_page.dart';
 import 'package:parking_project/presentation/theme/app_colors.dart';
 import 'package:parking_project/presentation/theme/theme.dart';
 
@@ -25,30 +26,6 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.background,
-      appBar: AppBar(
-        title: const Text('App title'),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Text('Welcome my to this event my friends', style: Theme.of(context).textTheme.bodyLarge,),
-              const SizedBox(height: 20,),
-              TextField(
-                onTapOutside: (_) => FocusScope.of(context).unfocus(),
-                decoration: const InputDecoration(
-                  labelText: 'Pls enter something'
-                ),
-              ),
-              const SizedBox(height: 10,),
-              ElevatedButton(onPressed: (){}, child: const Text('Click me!'))
-            ],
-          ),
-        ),
-      )
-    );
+    return const LoginPage();
   }
 }
---
+++ lib/presentation/pages/login_page/login_page.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../theme/app_colors.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Widget companyIcon = SvgPicture.asset(
+      'assets/icons/company_icon.svg',
+      width: 35,
+      height: 35,
+    );
+
+    final Widget carImage = SvgPicture.asset(
+      'assets/images/car_image.svg'
+    );
+
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      appBar: AppBar(
+        leading: Center(child: companyIcon),
+        title: const Text('Авторизация'),
+      ),
+      body: Column(
+        children: [
+           Expanded(
+            flex: 2,
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: carImage,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              height: double.maxFinite,
+              width: double.maxFinite,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(32),
+                  topRight: Radius.circular(32),
+                ),
+              ),
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text('Добро пожаловать!', style: Theme.of(context).textTheme.titleLarge, textAlign: TextAlign.center,),
+                      const SizedBox(height: 30,),
+                      TextField(
+                        textInputAction: TextInputAction.next,
+                        onTapOutside: (_) => FocusScope.of(context).unfocus(),
+                        decoration: InputDecoration(
+                          prefixIconColor: AppColors.primaryBlue,
+                          prefixIcon: const Padding(
+                            padding: EdgeInsets.only(left: 8.0,),
+                            child: Icon(Icons.mail),
+                          ),
+                          labelText: 'Введите почту'
+                        ),
+                      ),
+                      const SizedBox(height: 16,),
+                      TextField(
+                        textInputAction: TextInputAction.next,
+                        onTapOutside: (_) => FocusScope.of(context).unfocus(),
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            prefixIconColor: AppColors.primaryBlue,
+                            prefixIcon: const Padding(
+                              padding: EdgeInsets.only(left: 8.0,),
+                              child: Icon(Icons.lock),
+                            ),
+                            labelText: 'Введите пароль'
+                        ),
+                      ),
+                      const SizedBox(height: 16,),
+                      const Align(child: Text('Забыли пароль?'), alignment: Alignment.centerRight,),
+                      const SizedBox(height: 16,),
+                      ElevatedButton(onPressed: (){}, child: const Text('Войти')),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
