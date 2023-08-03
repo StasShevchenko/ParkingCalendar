95ebfeafbf347ed50cbcaa3c78de6731406155d6
StasShevchenko <79803411+StasShevchenko@users.noreply.github.com>
2023-08-03 11:39:54 +0300

    Now device configuration settings don't affect on textScale and screen orientation (#8)

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:parking_project/assets/themes/theme.dart';
 import 'package:parking_project/presentation/navigation/router.dart';
 import 'package:parking_project/presentation/pages/user/home_page/home_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   initializeDateFormatting('ru').then((_) => runApp( const MyApp()
   ));
-  runApp(const MyApp());
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((value) => runApp(const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -20,6 +24,12 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: appTheme(),
       routerConfig: goRouter(),
+      builder: (context, child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+          child: child!,
+        );
+      },
     );
   }
 }
