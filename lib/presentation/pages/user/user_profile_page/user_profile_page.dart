f652a325a065ceb79087df7c8477822037ea0176
IliaMuhtarov <90609655+IliaMuhtarov@users.noreply.github.com>
2023-08-17 19:52:28 +0300

    Update user profile page

--- lib/presentation/pages/user/user_profile_page/user_profile_page.dart
+++ lib/presentation/pages/user/user_profile_page/user_profile_page.dart
@@ -1,7 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:parking_project/data/models/user_info.dart';
-import 'package:parking_project/presentation/pages/user/home_page/calendar_section.dart';
-import 'package:parking_project/presentation/pages/user/home_page/queue_section.dart';
+import 'package:flutter/widgets.dart';
+
 
 import '../../../../assets/colors/app_colors.dart';
 
@@ -15,15 +15,24 @@ class UserProfilePage extends StatefulWidget {
 
 class _UserProfilePageState extends State<UserProfilePage>  {
 
+  String? _intext;
+  TextEditingController inputtextField = TextEditingController();
+  void _processText() {
+
+    setState(() {
+      _intext = inputtextField.text;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+
     return  Scaffold(
         backgroundColor: AppColors.background,
         appBar: AppBar(
           backgroundColor: AppColors.primaryBlue,
           toolbarHeight: 55,
-          title: Text("Личный кабинет"),
+          title: const Text("Личный кабинет"),
           ),
         body: Column (
           children: [
@@ -43,37 +52,66 @@ class _UserProfilePageState extends State<UserProfilePage>  {
               style: Theme.of(context).textTheme.titleMedium,
               textAlign: TextAlign.center,
             ),
-            TextField(
-              decoration: InputDecoration(
+            Padding(
+              padding: const EdgeInsets.all(19.0),
+              child: TextField(
+                controller: inputtextField,
+                decoration: InputDecoration(
 
-                prefixIcon: Icon(Icons.lock),
-                prefixIconColor: AppColors.primaryBlue,
+                  prefixIcon: const Icon(Icons.lock),
+                  prefixIconColor: AppColors.primaryBlue,
 
-                labelText: 'Ваш пароль',
-                labelStyle: TextStyle(color: AppColors.primaryBlue),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: AppColors.primaryBlue,
-                    width: 2.0
+                  labelText: 'Ваш пароль',
+                  labelStyle: TextStyle(color: AppColors.primaryBlue),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: AppColors.primaryBlue,
+                      width: 2.0
+                    ),
+                    borderRadius: BorderRadius.circular(32),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                   color: AppColors.primaryBlue,
+                  width: 2.0
                   ),
                   borderRadius: BorderRadius.circular(32),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                 color: AppColors.primaryBlue,
-                width: 2.0
-                ),
-                borderRadius: BorderRadius.circular(32),
+                  ),
                 ),
               ),
             ),
+
+            ElevatedButton (
+             child: const Text('Сохранить изменения'),
+
+              onPressed: () {
+                _processText();
+                showDialog<String>(
+                  context: context,
+                  builder: (BuildContext context) =>
+                      AlertDialog(
+                        title: const Text('Подтвердите смену пароля'),
+                        content: Text(
+                            'Вы уверены, что хотите сменить пароль на "$_intext"'),
+
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () => Navigator.pop(context, 'Отмена'),
+                            child: const Text('Отмена'),
+                          ),
+                          TextButton(
+                            onPressed: () =>
+                                Navigator.pop(context, 'Подтвердить'),
+                            child: const Text('Подтвердить'),
+                          ),
+                        ],
+                      ),
+                );
+              }
+            ),
           ],
         ),
-
-
-        );
+    );
   }
   }
-
-
 
