c635f14cf7792ff4c1db0c17dda874e18b2bc1b8
IliaMuhtarov <90609655+IliaMuhtarov@users.noreply.github.com>
2023-08-18 14:17:29 +0300

    Update user profile page

--- lib/presentation/pages/user/user_profile_page/user_profile_page.dart
+++ lib/presentation/pages/user/user_profile_page/user_profile_page.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 
 import '../../../../assets/colors/app_colors.dart';
@@ -15,12 +13,12 @@ class UserProfilePage extends StatefulWidget {
 
 class _UserProfilePageState extends State<UserProfilePage>  {
 
-  String? _intext;
-  TextEditingController inputtextField = TextEditingController();
+  String? _passwordText;
+  TextEditingController passwordFieldController = TextEditingController();
   void _processText() {
 
     setState(() {
-      _intext = inputtextField.text;
+      _passwordText = passwordFieldController.text;
     });
   }
 
@@ -47,40 +45,38 @@ class _UserProfilePageState extends State<UserProfilePage>  {
                 ),
               ),
             ),
+
             Text(
               'Ваш пароль:',
               style: Theme.of(context).textTheme.titleMedium,
               textAlign: TextAlign.center,
             ),
-            Padding(
-              padding: const EdgeInsets.all(19.0),
+            const SizedBox(
+              height: 16,
+            ),
+            ConstrainedBox(
+              constraints: const BoxConstraints(
+                  maxWidth: 350
+              ),
               child: TextField(
-                controller: inputtextField,
+                controller: passwordFieldController,
+                textInputAction: TextInputAction.done,
+                onTapOutside: (_) => FocusScope.of(context).unfocus(),
+                obscureText: false,
                 decoration: InputDecoration(
-
-                  prefixIcon: const Icon(Icons.lock),
-                  prefixIconColor: AppColors.primaryBlue,
-
-                  labelText: 'Ваш пароль',
-                  labelStyle: TextStyle(color: AppColors.primaryBlue),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: AppColors.primaryBlue,
-                      width: 2.0
+                    prefixIconColor: AppColors.primaryBlue,
+                    prefixIcon: const Padding(
+                      padding: EdgeInsets.only(
+                        left: 8.0,
+                      ),
+                      child: Icon(Icons.lock),
                     ),
-                    borderRadius: BorderRadius.circular(32),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                   color: AppColors.primaryBlue,
-                  width: 2.0
-                  ),
-                  borderRadius: BorderRadius.circular(32),
-                  ),
-                ),
+                    labelText: 'Ваш пароль'),
               ),
             ),
-
+            const SizedBox(
+              height: 16,
+            ),
             ElevatedButton (
              child: const Text('Сохранить изменения'),
 
@@ -92,7 +88,7 @@ class _UserProfilePageState extends State<UserProfilePage>  {
                       AlertDialog(
                         title: const Text('Подтвердите смену пароля'),
                         content: Text(
-                            'Вы уверены, что хотите сменить пароль на "$_intext"'),
+                            'Вы уверены, что хотите сменить пароль на "$_passwordText"'),
 
                         actions: <Widget>[
                           TextButton(
