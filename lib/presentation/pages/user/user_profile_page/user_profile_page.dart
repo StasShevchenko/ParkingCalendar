75e327be31c194ee360ccbeb80bc4c4f4ecaaba0
IliaMuhtarov <90609655+IliaMuhtarov@users.noreply.github.com>
2023-08-21 12:56:53 +0300

    Update user profile page

--- lib/presentation/pages/user/user_profile_page/user_profile_page.dart
+++ lib/presentation/pages/user/user_profile_page/user_profile_page.dart
@@ -15,8 +15,8 @@ class _UserProfilePageState extends State<UserProfilePage>  {
 
   String? _passwordText;
   TextEditingController passwordFieldController = TextEditingController();
-  void _processText() {
 
+  void _processText() {
     setState(() {
       _passwordText = passwordFieldController.text;
     });
@@ -32,81 +32,84 @@ class _UserProfilePageState extends State<UserProfilePage>  {
           toolbarHeight: 55,
           title: const Text("Личный кабинет"),
           ),
-        body: Column (
-          children: [
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+              child: Column (
+                children: [
 
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Center(
-                child: Text(
-                  'Добро пожаловать, Михаил!',
-                  selectionColor: AppColors.primaryBlue,
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-              ),
-            ),
+                  Text(
+                    'Добро пожаловать, Михаил!',
+                    selectionColor: AppColors.primaryBlue,
+                    style: Theme.of(context).textTheme.titleMedium,
+                    textAlign: TextAlign.center,
+                  ),
 
-            Text(
-              'Ваш пароль:',
-              style: Theme.of(context).textTheme.titleMedium,
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            ConstrainedBox(
-              constraints: const BoxConstraints(
-                  maxWidth: 350
-              ),
-              child: TextField(
-                controller: passwordFieldController,
-                textInputAction: TextInputAction.done,
-                onTapOutside: (_) => FocusScope.of(context).unfocus(),
-                obscureText: false,
-                decoration: InputDecoration(
-                    prefixIconColor: AppColors.primaryBlue,
-                    prefixIcon: const Padding(
-                      padding: EdgeInsets.only(
-                        left: 8.0,
-                      ),
-                      child: Icon(Icons.lock),
+                  Text(
+                    'Ваш пароль:',
+                    style: Theme.of(context).textTheme.titleMedium,
+                    textAlign: TextAlign.center,
+                  ),
+
+                  const SizedBox(
+                    height: 16,
+                  ),
+
+                  ConstrainedBox(
+                    constraints: const BoxConstraints(
+                        maxWidth: 350
                     ),
-                    labelText: 'Ваш пароль'),
+                    child: TextField(
+                      controller: passwordFieldController,
+                      textInputAction: TextInputAction.done,
+                      onTapOutside: (_) => FocusScope.of(context).unfocus(),
+                      obscureText: false,
+                      decoration: InputDecoration(
+                          prefixIconColor: AppColors.primaryBlue,
+                          prefixIcon: const Padding(
+                            padding: EdgeInsets.only(
+                              left: 8.0,
+                            ),
+                            child: Icon(Icons.lock),
+                          ),
+                          labelText: 'Новый пароль'),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  ElevatedButton (
+                   child: const Text('Сохранить изменения'),
+                    onPressed: () {
+                      _processText();
+                      showDialog<String>(
+                        context: context,
+                        builder: (BuildContext context) =>
+                            AlertDialog(
+                              title: const Text('Подтвердите смену пароля'),
+                              content: Text(
+                                  'Вы уверены, что хотите сменить пароль на "$_passwordText"'),
+
+                              actions: <Widget>[
+                                TextButton(
+                                  onPressed: () => Navigator.pop(context, 'Отмена'),
+                                  child: const Text('Отмена'),
+                                ),
+                                TextButton(
+                                  onPressed: () =>
+                                      Navigator.pop(context, 'Подтвердить'),
+                                  child: const Text('Подтвердить'),
+                                ),
+                              ],
+                            ),
+                        );
+                     }
+                  ),
+                ],
               ),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            ElevatedButton (
-             child: const Text('Сохранить изменения'),
-
-              onPressed: () {
-                _processText();
-                showDialog<String>(
-                  context: context,
-                  builder: (BuildContext context) =>
-                      AlertDialog(
-                        title: const Text('Подтвердите смену пароля'),
-                        content: Text(
-                            'Вы уверены, что хотите сменить пароль на "$_passwordText"'),
-
-                        actions: <Widget>[
-                          TextButton(
-                            onPressed: () => Navigator.pop(context, 'Отмена'),
-                            child: const Text('Отмена'),
-                          ),
-                          TextButton(
-                            onPressed: () =>
-                                Navigator.pop(context, 'Подтвердить'),
-                            child: const Text('Подтвердить'),
-                          ),
-                        ],
-                      ),
-                );
-              }
-            ),
-          ],
+          ),
         ),
+
     );
   }
   }
