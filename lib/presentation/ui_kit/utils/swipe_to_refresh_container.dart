77990c2b65dc3cb7cb92d949f9bb717a72f60d85
Stas <dark.knight22832@gmail.com>
2023-08-31 11:17:43 +0300

    Swipe to refresh container now calls on refresh function

--- lib/presentation/ui_kit/utils/swipe_to_refresh_container.dart
+++ lib/presentation/ui_kit/utils/swipe_to_refresh_container.dart
@@ -10,7 +10,7 @@ class SwipeToRefreshContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return RefreshIndicator(
-      onRefresh: () async => onRefresh,
+      onRefresh: () async => onRefresh(),
       child: CustomScrollView(
         physics: const AlwaysScrollableScrollPhysics(),
         slivers: [
