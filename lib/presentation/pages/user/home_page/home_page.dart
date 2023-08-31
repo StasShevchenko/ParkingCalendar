c6b6370164aa17b7dde02a475e2681cb2777b89d
Stas <dark.knight22832@gmail.com>
2023-08-31 10:16:48 +0300

    Swipe to refresh functionality is added

--- lib/presentation/pages/user/home_page/home_page.dart
+++ lib/presentation/pages/user/home_page/home_page.dart
@@ -20,7 +20,6 @@ class UserHomePage extends StatefulWidget {
 
 class _UserHomePageState extends State<UserHomePage>
     with SingleTickerProviderStateMixin {
-
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
@@ -30,7 +29,6 @@ class _UserHomePageState extends State<UserHomePage>
         builder: (context, state) {
           final bloc = context.read<HomePageBloc>();
           final userRoles = context.read<AuthCubit>().state.userData!.roles;
-
           if (state.isConnectionError) {
             return ConnectionErrorSection(onButtonClicked: () {
               bloc.add(PageRefreshed());
@@ -44,75 +42,89 @@ class _UserHomePageState extends State<UserHomePage>
           } else {
             return DefaultTabController(
               length: userRoles.contains(Role.User) ? 2 : 1,
-              child: Scaffold(
-                backgroundColor: AppColors.background,
-                appBar: MediaQuery.of(context).size.width < 880 &&
-                        userRoles.contains(Role.User)
-                    ? AppBar(
-                        backgroundColor: AppColors.primaryBlue,
-                        toolbarHeight: 0,
-                        bottom: TabBar(
-                          indicatorColor: AppColors.primaryWhite,
-                          labelColor: AppColors.primaryWhite,
-                          unselectedLabelColor: Colors.grey,
-                          tabs: const [
-                            Tab(
-                              text: 'Календарь',
-                            ),
-                            Tab(text: 'Очередь')
-                          ],
-                        ),
-                      )
-                    : null,
-                body: MediaQuery.of(context).size.width < 880
-                    ? TabBarView(
-                        children: [
-                          if (userRoles.contains(Role.User))
-                            CalendarSection(
-                              userInfo: state.userInfo!,
-                            ),
-                          QueueSection(
-                              isLoading: state.isQueueLoading,
-                              onSearchEntered: (value) {
-                                bloc.add(SearchEntered(searchQueue: value));
-                              },
-                              queueItems: state.queueItems!),
-                        ],
-                      )
-                    : Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          if (userRoles.contains(Role.User))
-                            Expanded(
-                              flex: 2,
-                              child: CalendarSection(
-                                userInfo: state.userInfo!,
-                              ),
-                            ),
-                          Expanded(
-                            flex: 3,
-                            child: Padding(
-                              padding:
-                                  const EdgeInsets.only(right: 32.0, top: 16.0),
-                              child: Center(
-                                child: ConstrainedBox(
-                                  constraints:
-                                      const BoxConstraints(maxWidth: 800),
-                                  child: QueueSection(
-                                    isLoading: state.isQueueLoading,
-                                    onSearchEntered: (value) {
-                                      bloc.add(
-                                        SearchEntered(searchQueue: value),
-                                      );
-                                    },
-                                    queueItems: state.queueItems!,
+              child: RefreshIndicator(
+                onRefresh: () async {
+                  bloc.add(PageRefreshed());
+                },
+                child: CustomScrollView(
+                    physics: const AlwaysScrollableScrollPhysics(),
+                    slivers: [
+                      SliverFillRemaining(
+                        child: Scaffold(
+                          backgroundColor: AppColors.background,
+                          appBar: MediaQuery.of(context).size.width < 880 &&
+                                  userRoles.contains(Role.User)
+                              ? AppBar(
+                                  surfaceTintColor: Colors.transparent,
+                                  backgroundColor: AppColors.primaryBlue,
+                                  toolbarHeight: 0,
+                                  bottom: TabBar(
+                                    indicatorColor: AppColors.primaryWhite,
+                                    labelColor: AppColors.primaryWhite,
+                                    unselectedLabelColor: Colors.grey,
+                                    tabs: const [
+                                      Tab(
+                                        text: 'Календарь',
+                                      ),
+                                      Tab(text: 'Очередь')
+                                    ],
                                   ),
+                                )
+                              : null,
+                          body: MediaQuery.of(context).size.width < 880
+                              ? TabBarView(
+                                  children: [
+                                    if (userRoles.contains(Role.User))
+                                      CalendarSection(
+                                        userInfo: state.userInfo!,
+                                      ),
+                                    QueueSection(
+                                        isLoading: state.isQueueLoading,
+                                        onSearchEntered: (value) {
+                                          bloc.add(SearchEntered(
+                                              searchQueue: value));
+                                        },
+                                        queueItems: state.queueItems!),
+                                  ],
+                                )
+                              : Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    if (userRoles.contains(Role.User))
+                                      Expanded(
+                                        flex: 2,
+                                        child: CalendarSection(
+                                          userInfo: state.userInfo!,
+                                        ),
+                                      ),
+                                    Expanded(
+                                      flex: 3,
+                                      child: Padding(
+                                        padding: const EdgeInsets.only(
+                                            right: 32.0, top: 16.0),
+                                        child: Center(
+                                          child: ConstrainedBox(
+                                            constraints: const BoxConstraints(
+                                                maxWidth: 800),
+                                            child: QueueSection(
+                                              isLoading: state.isQueueLoading,
+                                              onSearchEntered: (value) {
+                                                bloc.add(
+                                                  SearchEntered(
+                                                      searchQueue: value),
+                                                );
+                                              },
+                                              queueItems: state.queueItems!,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    )
+                                  ],
                                 ),
-                              ),
-                            ),
-                          )
-                        ],
+                        ),
                       ),
+                    ]),
               ),
             );
           }
