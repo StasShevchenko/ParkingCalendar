f88013f90c1b3697af1f65b185d37e06fe2c017c
Stas <dark.knight22832@gmail.com>
2023-08-31 10:40:30 +0300

    App bar shadowing on scroll down was fixed. Utils swipe to refresh container widget is created

---
+++ lib/presentation/ui_kit/utils/swipe_to_refresh_container.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SwipeToRefreshContainer extends StatelessWidget {
+  final Function onRefresh;
+  final Widget child;
+
+  const SwipeToRefreshContainer(
+      {super.key, required this.onRefresh, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return RefreshIndicator(
+      onRefresh: () async => onRefresh,
+      child: CustomScrollView(
+        physics: const AlwaysScrollableScrollPhysics(),
+        slivers: [
+          SliverFillRemaining(
+            child: child,
+          )
+        ],
+      ),
+    );
+  }
+}
