a90fe07320f77100a9d404c309d5774ad62f1276
Stas <dark.knight22832@gmail.com>
2023-09-23 11:02:53 +0300

    Forgot password short password message is changed

--- lib/presentation/pages/forgot_password_page/forgot_password_bloc/forgot_password_bloc.dart
+++ lib/presentation/pages/forgot_password_page/forgot_password_bloc/forgot_password_bloc.dart
@@ -58,7 +58,7 @@ class ForgotPasswordBloc
           if (isPasswordError) {
             emit(state.copyWith(
                 isPasswordError: true,
-                passwordErrorText: 'Длина не меньше 8 символов!',
+                passwordErrorText: 'Пароль должен содержать не менее 8 символов!',
                 isPasswordChangeLoading: false));
           } else if (state.password != state.repeatPassword) {
             emit(state.copyWith(
