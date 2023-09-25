493f3c4aa72ec98874be460ded55f07359719937
Stas <dark.knight22832@gmail.com>
2023-09-25 21:06:37 +0300

    Deleted user cannot enter the system with refresh token saved locally anymore

--- lib/presentation/auth_cubit/auth_cubit.dart
+++ lib/presentation/auth_cubit/auth_cubit.dart
@@ -1,6 +1,8 @@
 import 'package:bloc/bloc.dart';
+import 'package:dio/dio.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:parking_project/data/app_secure_storage.dart';
+import 'package:parking_project/data/remote_data_source/dio_configuration/dio_client.dart';
 
 import '../../../data/models/user.dart';
 import '../../data/remote_data_source/auth_data_source.dart';
@@ -10,25 +12,35 @@ part 'auth_state.dart';
 class AuthCubit extends Cubit<AuthState> {
   final AppSecureStorage _storage = AppSecureStorage();
   AuthRemoteDataSource authRemoteDataSource = AuthRemoteDataSource();
+  final dio = DioClient.get();
 
   AuthCubit() : super(AuthState()) {
     _storage.readRefreshToken().then(
-      (value) {
-        if (value != null) {
-          if (JwtDecoder.isExpired(value)) {
+      (value) async {
+        try {
+          if (value != null) {
+            if (JwtDecoder.isExpired(value)) {
+              emit(AuthState(
+                  authStatus: AuthStatus.unauthenticated, userData: null));
+            } else {
+              await dio
+                  .post('/token/refresh', data: {'refresh': value});
+              final json = JwtDecoder.decode(value);
+              final userData = User.fromJson(json['user']);
+              emit(
+                AuthState(
+                    authStatus: AuthStatus.authenticated, userData: userData),
+              );
+            }
+          } else {
             emit(AuthState(
                 authStatus: AuthStatus.unauthenticated, userData: null));
-          } else {
-            final json = JwtDecoder.decode(value);
-            final userData = User.fromJson(json['user']);
-            emit(
-              AuthState(
-                  authStatus: AuthStatus.authenticated, userData: userData),
-            );
           }
-        } else {
-          emit(AuthState(
-              authStatus: AuthStatus.unauthenticated, userData: null));
+        } on DioException catch (exception) {
+          if(exception.response?.statusCode == 404) {
+            emit(AuthState(
+                authStatus: AuthStatus.unauthenticated, userData: null));
+          }
         }
       },
     );
