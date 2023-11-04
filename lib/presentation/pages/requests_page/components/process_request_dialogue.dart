50235704310734594df821431245e17432c9550e
Stas <dark.knight22832@gmail.com>
2023-11-04 16:08:06 +0300

    Process request dialogue is created

---
+++ lib/presentation/pages/requests_page/components/process_request_dialogue.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:intl/intl.dart';
+import 'package:parking_project/data/models/request_dto.dart';
+
+class ProcessRequestDialogue extends StatelessWidget {
+  final bool isAgree;
+  final RequestDto requestInfo;
+  final DateFormat formatter = DateFormat('dd.MM.yyyy');
+
+  ProcessRequestDialogue({
+    super.key,
+    required this.isAgree,
+    required this.requestInfo,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Center(child: Text("Запрос на обмен", style: Theme.of(context).textTheme.titleMedium,)),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(isAgree ? "Вы действительно хотите подтвердить следующий обмен?"
+              : "Вы действительно хотите отказаться от следующего обмена?", softWrap: true,),
+          const SizedBox(height: 8,),
+          Text(
+              "Обмен: ${formatter.format(requestInfo.swapInfo.from)}(${requestInfo.sender.fullName})"
+                  " ⇔ "
+                  "${formatter.format(requestInfo.swapInfo.to)}(${requestInfo.receiver.fullName})", softWrap: true,),
+        ],
+      ),
+      actions: [
+        ElevatedButton(onPressed: (){context.pop();}, child: Text('Закрыть')),
+        SizedBox(width: 8,),
+        ElevatedButton(onPressed: (){}, child: Text('Подтвердить'))
+      ],
+    );
+  }
+}
--- lib/presentation/pages/requests_page/components/request_item.dart
+++ lib/presentation/pages/requests_page/components/request_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:parking_project/assets/colors/app_colors.dart';
 import 'package:parking_project/data/models/request_dto.dart';
+import 'package:parking_project/presentation/pages/requests_page/components/process_request_dialogue.dart';
 
 class RequestItem extends StatelessWidget {
   final RequestDto request;
@@ -24,41 +25,95 @@ class RequestItem extends StatelessWidget {
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                if (showSender != null && showSender == true) ...{
-                  Text(
-                      "Отправитель: ${request.sender.fullName} ${request.sender.email}"),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (showSender != null && showSender == true) ...{
+                    Text(
+                        "Отправитель: ${request.sender.fullName} ${request.sender.email}"),
+                    const SizedBox(
+                      height: 8,
+                    )
+                  },
+                  if (showReceiver != null && showReceiver == true) ...{
+                    Text(
+                        "Получатель: ${request.receiver.fullName} ${request.sender.email}"),
+                    const SizedBox(
+                      height: 8,
+                    )
+                  },
+                  Text("Отправлено: ${formatter.format(request.sent)}"),
                   const SizedBox(
                     height: 8,
-                  )
-                },
-                if (showReceiver != null && showReceiver == true) ...{
+                  ),
                   Text(
-                      "Получатель: ${request.receiver.fullName} ${request.sender.email}"),
-                  const SizedBox(
-                    height: 8,
-                  )
-                },
-                Text("Отправлено: ${formatter.format(request.sent)}"),
-                const SizedBox(
-                  height: 8,
-                ),
-                Text(
-                    "Обмен: ${formatter.format(request.swapInfo.from)}(${request.sender.fullName})"
-                    " ⇔ "
-                    "${formatter.format(request.swapInfo.to)}(${request.receiver.fullName})"),
-                if(request.isActive == false)...{
-                  SizedBox(height: 8,),
-                  if(request.result == true)
-                   Text("Подтверждено", style: TextStyle(fontWeight: FontWeight.bold),),
-                  if(request.result == false)
-                    Text("Отклонено", style: TextStyle(fontWeight: FontWeight.bold),)
-                },
-              ],
+                      "Обмен: ${formatter.format(request.swapInfo.from)}(${request.sender.fullName})"
+                      " ⇔ "
+                      "${formatter.format(request.swapInfo.to)}(${request.receiver.fullName})", softWrap: true,),
+                  if (request.isActive == false) ...{
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    if (request.result == true)
+                      const Text(
+                        "Подтверждено",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    if (request.result == false)
+                      const Text(
+                        "Отклонено",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      )
+                  },
+                  if (request.isActive) ...{
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) => ProcessRequestDialogue(
+                                  isAgree: true, requestInfo: request),
+                            );
+                          },
+                          padding: EdgeInsets.zero,
+                          constraints: const BoxConstraints(),
+                          icon: const Icon(
+                            Icons.check_circle,
+                            color: Colors.greenAccent,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 16,
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) => ProcessRequestDialogue(
+                                  isAgree: false, requestInfo: request),
+                            );
+                          },
+                          padding: EdgeInsets.zero,
+                          constraints: const BoxConstraints(),
+                          icon: const Icon(
+                            Icons.do_disturb_alt_sharp,
+                            color: Colors.redAccent,
+                          ),
+                        ),
+                      ],
+                    )
+                  },
+                ],
+              ),
             ),
-            const SizedBox(width: 8,),
+            const SizedBox(
+              width: 8,
+            ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Expanded(
@@ -67,16 +122,14 @@ class RequestItem extends StatelessWidget {
                     height: 10,
                     width: 10,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(100),
-                      color: _getRequestResultColor(request),
-                      boxShadow: [
-                        BoxShadow(
-                          color: _getRequestResultColor(request),
-                          blurRadius: 4,
-                          spreadRadius: 2
-                        )
-                      ]
-                    ),
+                        borderRadius: BorderRadius.circular(100),
+                        color: _getRequestResultColor(request),
+                        boxShadow: [
+                          BoxShadow(
+                              color: _getRequestResultColor(request),
+                              blurRadius: 4,
+                              spreadRadius: 2)
+                        ]),
                   ),
                 ),
               ),
@@ -87,13 +140,13 @@ class RequestItem extends StatelessWidget {
     );
   }
 
-  Color _getRequestResultColor(RequestDto request){
-    if(request.isActive){
+  Color _getRequestResultColor(RequestDto request) {
+    if (request.isActive) {
       return AppColors.primaryBlue;
-    } else{
-      if(request.result == true){
+    } else {
+      if (request.result == true) {
         return Colors.greenAccent;
-      } else{
+      } else {
         return Colors.redAccent;
       }
     }
